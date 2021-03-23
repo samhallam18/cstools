ea75af8d825d585ea9a8452d726b3f99ccfbd51c
BuildTools <92414@farnborough.ac.uk>
2021-03-23 22:56:35 +0000

    updated index.js, added a table

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import IndexTable from './IndexTable';
+
+class Index extends Component {
+    render() {
+        return (
+            <div className="Index">
+                <h1>CS Tools</h1>
+                <IndexTable />
+            </div>
+        )
+    }
+}
+
+export default Index
---
+++ src/IndexTable.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class IndexTable extends Component {
+    render() {
+        return(
+            <table>
+                <tr>
+                    <th>Tool Name</th>
+                    <th>Description</th>
+                </tr>
+                <tr>
+                    <td>Binary Search</td>
+                    <td>This is the description.</td>
+                </tr>
+            </table>
+        )
+    }
+}
+
+export default IndexTable
