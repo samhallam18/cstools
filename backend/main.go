63b557377004b7d680274822af7299b42176973c
BuildTools <92414@farnborough.ac.uk>
2021-03-24 08:55:57 +0000

    started backend, wrote a simple api

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/api/test", func(con *gin.Context) {
+		con.JSON(http.StatusOK, gin.H{
+			"response": "This api link is temporary.",
+		})
+	})
+
+	router.Run(":3001")
+}
