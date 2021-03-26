c43e8585746f702ce1862d5cf56a610105dfdcf6
BuildTools <92414@farnborough.ac.uk>
2021-03-26 19:01:17 +0000

    added insertion sort

--- backend/sorts.go
+++ backend/sorts.go
@@ -11,6 +11,7 @@ func groupSorts(route *gin.Engine) {
 	sorts := route.Group("/sorts")
 
 	sorts.POST("/bubble", bubbleSort)
+	sorts.POST("/insertion", insertionSort)
 }
 
 func bubbleSort(con *gin.Context) {
@@ -38,4 +39,25 @@ func bubbleSort(con *gin.Context) {
 		"steps":    steps,
 	})
 
+}
+
+func insertionSort(con *gin.Context) {
+	items := strings.Split(con.PostForm("items"), ",")
+	steps := [][]string{}
+	for i := 1; i < len(items); i++ {
+		key := items[i]
+		j := i - 1
+		for j >= 0 && items[j] > key {
+			items[j+1] = items[j]
+			j = j - 1
+		}
+		items[j+1] = key
+		tempCopy := make([]string, len(items))
+		copy(tempCopy, items)
+		steps = append(steps, tempCopy)
+	}
+	con.JSON(http.StatusOK, gin.H{
+		"response": items,
+		"steps":    steps,
+	})
 }
